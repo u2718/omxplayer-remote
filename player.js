29135d0bbba1eae16531af29ac22c198ae6ea9fc
Lazarev Mikhail <lazarevm93@gmail.com>
2016-11-20 22:10:20 +0500

    Сохранение состояния воспроизведения

--- player.js
+++ player.js
@@ -3,14 +3,27 @@ var config = require('./config');
 
 var player = {
     process: null,
+    state: 'stop',
+    file: null,
+
     play: function play (path) {
         this.process = child_process.spawn(config.player, config.playerArgs.concat(path));
+        this.state = 'play';
+        this.file = path;
     },
     stop: function () {
         this.process.stdin.write('q');
+        this.state = 'stop';
+        this.file = null;
     },
     pause: function () {
-        this.process.stdin.write('p');
+        if (this.state == 'pause' || this.state == 'play') {
+            this.process.stdin.write('p');
+            this.state = this.state == 'play' ? 'pause' : 'play';
+        }
+        else {
+            throw new Error('Invalid state');
+        }
     }
 };
 
--- routes/play.js
+++ routes/play.js
@@ -3,8 +3,15 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/:file', function (req, res, next) {
-  req.player.play(req.params.file);
-  res.render('play', { title: 'Play', file: req.params.file });
+  var file;
+  if (req.player.state == 'stop') {
+    file = req.params.file;
+    req.player.play(file);
+  }
+  else {
+    file = req.player.file;
+  }
+  res.render('play', { title: 'Play', file: file });
 });
 
 module.exports = router;
