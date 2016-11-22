8ff89e6c62a0b4e8aac2436743cc551421886b13
Lazarev Mikhail <lazarevm93@gmail.com>
2016-11-22 21:59:21 +0500

    twitch.tv

--- config.js
+++ config.js
@@ -2,7 +2,8 @@ var config = {
     directories: [],
     extensions: ['mkv', 'avi', 'mp4'],
     player: 'omxplayer',
-    playerArgs: ['-o', 'hdmi', '-b']
+    playerArgs: ['-o', 'hdmi', '-b'],
+    twitchPlayer: 'livestreamer'
 };
 
 module.exports = config;
--- player.js
+++ player.js
@@ -1,13 +1,22 @@
 var child_process = require('child_process');
 var config = require('./config');
 
+var spawnPlayer = function (path) {
+    if (path.indexOf('twitch.tv') != -1) {
+        return child_process.spawn(config.twitchPlayer, [path])
+    }
+    else {
+        return child_process.spawn(config.player, config.playerArgs.concat(path));
+    }
+};
+
 var player = {
     process: null,
     state: 'stop',
     file: null,
 
     play: function play (path) {
-        this.process = child_process.spawn(config.player, config.playerArgs.concat(path));
+        this.process = spawnPlayer(path);
         this.state = 'play';
         this.file = path;
     },
