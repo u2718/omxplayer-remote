0658f74176427d0588aef489ae9347af70c4ebe9
Lazarev Mikhail <lazarevm93@gmail.com>
2016-12-11 21:30:34 +0500

    Переход на главную страницу после завершения работы плеера

--- player.js
+++ player.js
@@ -1,5 +1,6 @@
 var child_process = require('child_process');
 var config = require('./config');
+var is_running = require('is-running');
 
 var spawnPlayer = function (path) {
     if (path.indexOf('twitch.tv') != -1) {
@@ -15,24 +16,34 @@ var player = {
     state: 'stop',
     file: null,
 
-    play: function play (path) {
+    play: function play(path) {
         this.process = spawnPlayer(path);
         this.state = 'play';
         this.file = path;
     },
     stop: function () {
-        this.process.stdin.write('q');
+        this.send_command('q');
         this.state = 'stop';
         this.file = null;
     },
     pause: function () {
         if (this.state == 'pause' || this.state == 'play') {
-            this.process.stdin.write('p');
+            this.send_command('p');
             this.state = this.state == 'play' ? 'pause' : 'play';
         }
         else {
             throw new Error('Invalid state');
         }
+    },
+    is_running: function () {
+        return is_running(this.process.pid);
+    },
+    send_command: function (command) {
+        if (!this.is_running())
+            return false;
+
+        this.process.stdin.write(command);
+        return true;
     }
 };
 
--- routes/index.js
+++ routes/index.js
@@ -1,26 +1,30 @@
 var express = require('express');
 var shell = require('shelljs');
-var querystring = require('querystring')
+var querystring = require('querystring');
 var router = express.Router();
 
 var getFiles = function (root, extensions) {
-  var files = [];
-  shell.ls('-R', root).forEach(function (file) {
-    var ext = file.split('.').pop();
-    if (extensions.indexOf(ext) != -1){
-      var path = querystring.escape(root + '/' + file);
-      files.push({name: file.split('/').pop(), path: path});
-    }
-  });
-  return files;
+    var files = [];
+    shell.ls('-R', root).forEach(function (file) {
+        var ext = file.split('.').pop();
+        if (extensions.indexOf(ext) != -1) {
+            var path = querystring.escape(root + '/' + file);
+            files.push({name: file.split('/').pop(), path: path});
+        }
+    });
+    return files;
 };
 
-router.get('/', function(req, res, next) {
-  var list = [];
-  req.config.directories.forEach(function (dir) {
-    list = list.concat(getFiles(dir, req.config.extensions));
-  });
-  res.render('index', { title: 'List', list: list });
+router.get('/', function (req, res, next) {
+    var list = [];
+    req.config.directories.forEach(function (dir) {
+        list = list.concat(getFiles(dir, req.config.extensions));
+    });
+    res.render('index', {title: 'List', list: list});
+});
+
+router.get('/state', function (req, res, next) {
+    res.json({is_running: req.player.is_running()});
 });
 
 module.exports = router;
